615b556cd7f3d21948581935428fa1fee1feb662
Bemoi Erian <bemoi.erian@gmail.com>
2022-12-10 21:33:37 +0200

    added muxes of memory address

--- 4 - memory/memory_stage.v
+++ 4 - memory/memory_stage.v
@@ -38,35 +38,38 @@ output reg [15:0] MEMWB_ALU_result;
 output reg [2:0] MEMWB_Rdst_address;
 output reg MEMWB_memRead;
 output reg MEMWB_WB;
+reg newSP;
 reg CS;
-wire isMemory;
-wire [1:0] selPushOrPop;
+wire [15:0] dataFromMemoryWire;
+// wire isMemory;
 wire sp_push;
 wire sp_pop;
-wire [1:0] selAddress;
 wire [31:0] address;
-wire pushOrPop;
-wire [15:0] dataFromMemoryWire;
-assign isMemory = memRead | memWrite;
-// assign selPushOrPop = {push, pop};
-// // Mux to make the signal 0 if push, 1 if pop, else z
-// assign pushOrPop = 
-//     (selPushOrPop == 2'b01) ? 1'b1 : //pop
-//     (selPushOrPop == 2'b10) ? 1'b0 : //push 
-//     1'bz;
-// assign sp_push = sp + 1;
-// assign sp_pop = sp;
-// assign selAddress = {isMemory, pushOrPop};
-// assign address =
-//     (selAddress == 2'b00) ? Rdst_value : //memory but no push or pop
-//     (selAddress == 2'b01) ? 16'bz : //Error: no push or pop without memory
-//     (selAddress == 2'b10) ? sp_push : //memory + push 
-//     (selAddress == 2'b11) ? sp_pop : //memory + pop 
-//    16'bz;
-assign selAddress = {memRead,memWrite};
-assign address =                        //memory but no push or pop
-    (selAddress == 2'b01) ? {16'b0,Rdst_value} : //Error: no push or pop without memory
-    (selAddress == 2'b10) ? {16'b0,Rsrc_value} : 32'bz; //memory + push 
+
+wire sel_stackOrMem;
+wire sel_pushOrPop;
+wire sel_readOrWrite;
+wire [31:0] address_pushOrPop;
+wire [31:0] address_readOrWrite;
+// assign isMemory = memRead | memWrite;
+
+assign sp_push = sp;
+assign sp_pop = sp + 1;
+// MUX to select memory address in case we have memRead or memWrite
+assign sel_readOrWrite = memRead;
+assign address_readOrWrite =
+    (sel_readOrWrite == 1'b0) ? {16'b0,Rsrc_value} : //memWrite
+    (sel_readOrWrite == 1'b1) ? {16'b0,Rdst_value} : 32'bz; //memRead 
+// MUX to select memory address in case we have push or pop
+assign sel_pushOrPop = pop;
+assign address_pushOrPop =
+    (sel_pushOrPop == 1'b0) ? sp_push : //push
+    (sel_pushOrPop == 1'b1) ? sp_pop : 32'bz; //pop 
+// MUX to select memory address in case we have push/pop or memRead/memWrite
+assign sel_stackOrMem = !push & !pop;
+assign address =
+    (sel_stackOrMem == 1'b0) ? address_pushOrPop :
+    (sel_stackOrMem == 1'b1) ? address_readOrWrite : 32'bz;
     //(selAddress == 2'b11) ? sp_pop : //memory + pop 
 DataMemory mem(address, Rsrc_value, dataFromMemoryWire, memRead, memWrite, 1'b1, clk);
 always @(posedge clk) begin
@@ -79,5 +82,17 @@ always @(posedge clk) begin
     // if isMemory = 1, set CS (chip select) of memory to 1, else CS = 0
     //CS = 1;
     dataFromMemory = dataFromMemoryWire;
+    // update sp in case of push or pop
+    if (sel_stackOrMem == 0) begin
+        if (push) begin
+            newSP = sp - 1;
+        end
+        else begin
+            newSP = sp_pop;
+        end
+    end
+    else begin
+        newSP = sp;
+    end
 end
 endmodule
